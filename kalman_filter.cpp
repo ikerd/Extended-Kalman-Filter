6867ab5a15572879313c45b4daf5465a7232ed9c
Iker Diez <32014358+ikerd@users.noreply.github.com>
2018-04-27 09:04:31 +0200

    Add files via upload

---
+++ kalman_filter.cpp
@@ -0,0 +1,96 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in; // object state
+  P_ = P_in; // object covariance matrix
+  F_ = F_in; // state transition matrix
+  H_ = H_in; // measurement matrix
+  R_ = R_in; // measurement covariance matrix
+  Q_ = Q_in; // process covariance matrix
+}
+
+// The Kalman filter predict function. The same for linear and extended Kalman filter
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float rho = sqrt(px*px + py*py);
+  if(rho < 0.0001)  {
+    px += 0.001;
+    py += 0.001;
+    rho = sqrt( px*px + py*py);
+  }
+
+  float phi = atan2(py, px);
+  float rho_dot;
+  if (fabs(rho) < 0.0001) {
+    rho_dot = 0;
+  } else {
+    rho_dot = (px*vx + py*vy)/rho;
+  }
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+  VectorXd y = z - z_pred;
+  float PI = 3.14159265;
+  // Angle normalization as stated in project tips & tricks
+  if( y(1) > PI ){
+    y(1) -= 2.f*PI;}
+  if( y(1) < - PI ){
+    y(1) += 2.f*PI;}
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
